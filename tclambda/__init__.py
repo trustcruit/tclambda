b982c3787b7684a5432e29927d195b7aad480c7c
Niels Lemmens <draso.odin@gmail.com>
2019-06-13 10:01:48 +0200

    Bump version from v0.0.7 to v0.0.8

--- tclambda/__init__.py
+++ tclambda/__init__.py
@@ -4,5 +4,5 @@ from .exceptions import RetryException
 from .function import LambdaFunction
 from .handlers import LambdaHandler
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __all__ = ("LambdaFunction", "LambdaHandler", "RetryException")
